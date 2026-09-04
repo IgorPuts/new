cce5c89b5624c1d781e594b5cd6d21d9daf6a388
IgorPuts <exNeron@yandex.ru>
2026-09-04 23:12:28 +0300

    feat: some memories

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
-const colour = document.querySelector('p');
+
+
+const pColor = document.querySelector('p');
 const newColour = function () {
-	colour.style.color = 'blue';
+	pColor.style.color = 'blue';
 }
+newColour();
+
+const div = document.createElement('div');
+div.style.width = '100px';
+div.style.height = '100px';
+div.style.border = "2px dotted black";
+pColor.insertAdjacentElement("beforebegin", div);
+
+let top1;
+let left1;
+
+const moving = function () {
+
+	top1 = div.offsetTop;
+	left1 = div.offsetLeft;
+	div.style.left = (left1 + 30) + 'px';
+	div.style.top = (top1 + 30) + 'px';
+	if (top1 >= 270) {
+		clearInterval(move);
+	}
+
+}
+
+const move = setInterval(moving, 1000);
+
